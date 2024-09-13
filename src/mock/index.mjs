e2d3214446d219d57078c4f01dc338896f2cbf51
Manly Zhang <manly@stonememory.com>
2024-09-13 17:32:30 +0800

    feat: change ports

--- src/mock/index.mjs
+++ src/mock/index.mjs
@@ -3,7 +3,7 @@ import cors from "cors";
 import data from "./data.json" assert { type: "json" };
 
 const app = express();
-const port = 4000;
+const port = 4001;
 app.use(cors());
 app.get("/me", (req, res) => {
 	res.json(data.user);
